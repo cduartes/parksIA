8abeb7bbaea33ba69c7fe4a495b711d4df6d17df
cduartes <c.duartes.dn@gmail.com>
2018-04-15 17:51:42 -0300

    change print format

--- parks.py
+++ parks.py
@@ -11,30 +11,30 @@ class node():
         sons_list = []
         for coords in np.argwhere(self.matrix == 0):
             son = self.matrix.copy()
-            son[coords[0]] = -1                 # block horizontal
-            son.transpose()[coords[1]] = -1     # block vertical
+            son[coords[0]] = -1                                 # block horizontal
+            son.transpose()[coords[1]] = -1                     # block vertical
             shape = son.shape[0]
             for x in [-1, 1]:
                 for y in [-1, 1]:
                     if coords[0]+x > -1 and coords[1]+y > -1:
                         try:
-                            son[coords[0]+x][coords[1]+y] = -1 # block diagonals
+                            son[coords[0]+x][coords[1]+y] = -1  # block diagonals
                         except Exception as e:
                             pass
-            son[coords[0]][coords[1]] = 1 # put tree
+            son[coords[0]][coords[1]] = 1                       # put tree
             sons_list.append(node(son,self))
         return sons_list
 
 def is_solution(q_matrix,board, n):
     for x in q_matrix:
-        count = len(np.argwhere(x == 1)) # count where's a tree /horizontal
+        count = len(np.argwhere(x == 1))    # count where's a tree /horizontal
         if count > 1:
             return False
     for x in q_matrix.transpose():
-        count = len(np.argwhere(x == 1)) # count where's a tree /vertical
+        count = len(np.argwhere(x == 1))    # count where's a tree /vertical
         if count > 1:
             return False
-    for x in range(1, n+1):
+    for x in range(1, n+1):                 # count trees on parks
         count = 0
         for y in np.argwhere(board == x):
             if q_matrix[y[0]][y[1]] == 1:
@@ -66,28 +66,27 @@ if __name__ == "__main__":
     # Execution parameters
     parser = argparse.ArgumentParser()
     parser.add_argument("--strategy", "-str", type=str, required=True)      # dfs or bfs
-    parser.add_argument("--showstate", "-show", type=str, required=True)    # yes or no
+    parser.add_argument("--showstate", "-show", type=str, required=True)    # yes or no / y or n
     parser.add_argument("--sides", "-n", type=str, required=True)           # number
     args = parser.parse_args()
     args.showstate = args.showstate.lower()
     args.strategy = args.strategy.lower()
-    print("strategy   : "+args.strategy)
-    print("show state : "+args.showstate)
-    print("n sides    : "+args.sides)
+    print("Estrategia     : "+args.strategy)
+    print("Mostrar estado : "+args.showstate)
+    print("n lados        : "+args.sides)
     if(args.showstate == 'yes' or args.showstate == 'y'):
         show = True
-    else:
+    elif(args.showstate == 'no' or args.showstate == 'n'):
         show = False
 
     if(args.strategy not in ['bfs','dfs']):
         exit()
     n = int(args.sides)
-    root = node(np.array([[0 for x in range(n)] for y in range(n)]),
-                None)
+    root = node(np.array([[0 for x in range(n)] for y in range(n)]),None)   # generate root
     board = load_board(n)
-    print("tablero a resolver:")
-    print(board)
-    # State list    
+    print("Tablero a resolver:")
+    print('\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in board]))
+    # State list
     Q = [root]
     # Shape verification
     if(not(board.shape == root.matrix.shape) and root.matrix.shape[0] == n): 
@@ -96,20 +95,33 @@ if __name__ == "__main__":
 
     while(len(Q)):
         if(show): print("Largo de Q: "+str(len(Q)))
-
         q = Q.pop()
-
         if(is_solution(q.matrix, board,n)):
-            print("Solucion: ")
-            matrix_return = q.matrix.copy()
-            for coords in np.argwhere(q.matrix == -1):
-                matrix_return[coords[0],coords[1]] = 0
-            print(matrix_return)
-            print("Resuelto en: {0} seg.: ".format(time.time() - time_start))
-            exit()
-        
-        sons = q.generate_sons()
-        sons.reverse() # swap childrens place
+            print("\n")
+            aux = q
+            chain = []
+            while(aux.father):
+                chain.append(aux)
+                aux = aux.father
+            chain.reverse()
+
+            print("Cadena solucion (de padre a hijo)")
+            for cell in chain:
+                matrix_return = cell.matrix.copy()
+                for coords in np.argwhere(cell.matrix == -1):
+                    matrix_return[coords[0],coords[1]] = 0
+                print("-----")
+                print('\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in matrix_return]))
+            print("Es solucion del tablero.\nResuelto en: {0} seg.: ".format(time.time() - time_start))
+            exit()        
+        sons = q.generate_sons()    # generate sons
+        if(show and len(Q) > 0):
+            print("Nodos hijos:")
+            for i in [0,len(Q)]:
+                print("-----")
+                print('\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in Q[i].matrix]))
+                
+        sons.reverse()              # swap childrens place
 
         if(args.strategy == 'dfs'):
             # deep search strategy
